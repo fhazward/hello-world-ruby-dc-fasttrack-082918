5cbf8b201eade7d2fd2d6308701bc70e9ff4f688
comic-range-3517 <fhazward@gmail.com>
2018-09-29 15:50:54 +0000

    automatically backed up by learn

---
+++ hello_world.rb
@@ -0,0 +1,5 @@
+puts "Hello, I'm Rob."
+puts "I'm from Washington,DC."
+puts "welcome to the Flatiron Programming Fastrack"
+puts "I'm super excited to meet you, and learn some ruby!"
+puts "Hello World"
